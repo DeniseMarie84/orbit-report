d49478c95794fb758d2890d5010d3a0061662468
Denise Williams <denise.marie.84@gmail.com>
2019-12-09 21:00:45 -0600

    step 6 complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
+
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name= name,
     this.type= type,
@@ -11,4 +12,11 @@ constructor(name: string, type: string, launchDate: string, orbitType: string, o
     this.orbitType= orbitType,
     this.operational= operational
 };
+    shouldShowWarning(){
+        if (this.type.toLowerCase() === "space debris"){
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
